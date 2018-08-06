7bac9bbc956d5523a1ff9cac03753e8c8cdefc22
Guilherme Machado <guivmachado@gmail.com>
2018-08-06 22:06:17 +0100

    New Models

--- Simple_Linear_Regression/simple_linear_regression.py
+++ Simple_Linear_Regression/simple_linear_regression.py
@@ -40,7 +40,7 @@ plt.show()
 
 # Visualising the Test set results
 plt.scatter(X_test, y_test, color = 'red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.plot(X_test, y_pred, color = 'blue')
 plt.title('Salary vs Experience (Test set)')
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
